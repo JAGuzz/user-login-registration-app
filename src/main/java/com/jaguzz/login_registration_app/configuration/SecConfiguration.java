6c52a02206e73a4d7a9da3032284f29eaa641105
junkSquid <ing.agc98@gmail.com>
2025-02-26 16:37:35 -0700

    doble container version: esta version de la app esta diseñada para funcionar en 2 contenedores distintos

--- src/main/java/com/jaguzz/login_registration_app/configuration/SecConfiguration.java
+++ src/main/java/com/jaguzz/login_registration_app/configuration/SecConfiguration.java
@@ -1,7 +1,6 @@
 package com.jaguzz.login_registration_app.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
--- src/main/java/com/jaguzz/login_registration_app/service/UserService.java
+++ src/main/java/com/jaguzz/login_registration_app/service/UserService.java
@@ -1,10 +1,12 @@
 package com.jaguzz.login_registration_app.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
 import com.jaguzz.login_registration_app.model.User;
 import com.jaguzz.login_registration_app.web.dto.UserRegistrationDto;
 
+@Service
 public interface UserService extends UserDetailsService{
 
     User save(UserRegistrationDto registrationDto);
